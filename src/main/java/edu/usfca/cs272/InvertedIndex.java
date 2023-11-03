b66d8c46a0078cd74a731ede90e8394563f57c71
Anton Lim <anlim2@dons.usfca.edu>
2023-11-02 21:43:53 -0700

    removed unneeded imports

--- src/main/java/edu/usfca/cs272/InvertedIndex.java
+++ src/main/java/edu/usfca/cs272/InvertedIndex.java
@@ -1,11 +1,9 @@
 package edu.usfca.cs272;
 
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;

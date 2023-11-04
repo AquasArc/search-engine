62df1e707543e970d0a209f81cf7aab000a78e14
Anton Lim <anlim2@dons.usfca.edu>
2023-11-03 21:17:09 -0700

    if directory check in processquery

--- src/main/java/edu/usfca/cs272/QueryProcessor.java
+++ src/main/java/edu/usfca/cs272/QueryProcessor.java
@@ -54,8 +54,8 @@ public class QueryProcessor {
 	 * @throws IOException
 	 */
 	public Map<String, List<FileResult>> processQuery(Path queryPath, boolean isPartial) throws IOException {
-		if (queryPath == null) throw new IllegalArgumentException();
-		
+		if (queryPath == null || Files.isDirectory(queryPath)) throw new IllegalArgumentException();
+
 		List<String> queries = readQueries(queryPath);
 
 		for (String query : queries) {
